471e4a131d156e30b0cc94e9156da58ef958dd28
skyscraper <kwikius@yahoo.com>
2013-02-13 11:52:42 +0000

    now using Makefile to specify version so remove header board #define

--- config.hpp
+++ config.hpp
@@ -20,16 +20,18 @@
  along with this program. If not, see <http://www.gnu.org/licenses/>
 */
 
+// need one or the other
+// see resources.hpp for different pinouts 
 
 //#define MAVLINK_TO_FRSKY_STM32f4_DISCOVERY_BOARD
-#define MAVLINK_TO_FRSKY_TARGET_BOARD
-
-#if defined( MAVLINK_TO_FRSKY_STM32f4_DISCOVERY_BOARD) && defined(MAVLINK_TO_FRSKY_TARGET_BOARD)
- #error only one target board should be defined
-#endif
+//#define MAVLINK_TO_FRSKY_TARGET_BOARD
 
 #if !defined( MAVLINK_TO_FRSKY_STM32f4_DISCOVERY_BOARD) && !defined(MAVLINK_TO_FRSKY_TARGET_BOARD)
 #error either discovery board or target board must be defined
+#endif
+
+#if defined( MAVLINK_TO_FRSKY_STM32f4_DISCOVERY_BOARD) && defined(MAVLINK_TO_FRSKY_TARGET_BOARD)
+ #error only one target board should be defined
 #endif
 
 #endif // MAVLINK_TO_FRSKY_CONFIG_HPP_INCLUDED
